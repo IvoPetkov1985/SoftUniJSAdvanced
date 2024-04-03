07c2be869f8d1b9900fe74dde134cd8a2e945cfa
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-04-03 12:36:46 +0300

    First task added.
    Elementary printing on the console.

---
+++ Syntax, Functions and Statements - Lab/01. Echo Function.js
@@ -0,0 +1,9 @@
+function echoFunction(input) {
+    let stringLength = input.length;
+
+    console.log(stringLength);
+    console.log(input);
+}
+
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
